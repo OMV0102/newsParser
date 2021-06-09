277217a1c65f8acbe6754d3e5ccea67870f85643
omv0102 <omv98@outlook.com>
2021-06-10 01:18:03 +0700

    Спустя 4 часа наконец-то данные получены и залиты в таблицы. люблю python.

--- newsParser/newsParser.py
+++ newsParser/newsParser.py
@@ -15,7 +15,7 @@ class News:
     news_date: str
 
 @dataclass
-class teachers:
+class Employee:
     """Структура сотрудника"""
     idperson: int = 0
     name: str = ''
@@ -25,7 +25,6 @@ class teachers:
     chair_id: int = 0
     chair_id2: int = 0
     id: int = 0
-    create_ts: datetime = datetime.now()
 
 
 
@@ -38,21 +37,37 @@ linkPerson = "https://ciu.nstu.ru/kaf/persons/" # ссылка на страни
 newsYear = '2021'
 
 listNews = []
-
-response = requests.get(apiNews+newsYear)
+listEmployee = []
+#==================================================================================
+#response = requests.get(apiNews+newsYear)
+#if response.status_code == 200:
+#    jsonList = response.json()
+#    if len(jsonList) > 0:
+#        for jsonElem in jsonList:
+#            listElem = News(jsonElem['ID'], jsonElem['URL'], jsonElem['TITLE'], jsonElem['TEXT'], jsonElem['SHORTTEXT'], jsonElem['NEWS_DATE'])
+#            listNews.append(listElem)
+#    else:
+#        print('За ' + newsYear + ' год новостей нет!')
+#        sys.exit(1)
+#else:
+#    print('Ошибка: api новостей вернуло код: ' + response.status_code)
+#    sys.exit(1)
+#==================================================================================
+param = {apiHeader: apiKey,}
+response = requests.get(apiEmployee, headers={apiHeader:apiKey})
 if response.status_code == 200:
     jsonList = response.json()
     if len(jsonList) > 0:
         for jsonElem in jsonList:
-            listElem = News(jsonElem['ID'], jsonElem['URL'], jsonElem['TITLE'], jsonElem['TEXT'], jsonElem['SHORTTEXT'], jsonElem['NEWS_DATE'])
-            listNews.append(listElem)
+            listElem = Employee(jsonElem['IDPERSON'], jsonElem['NAME'], jsonElem['SURNAME'], jsonElem['PATRONYMIC'], jsonElem['POST'], jsonElem['CHAIR_ID'], jsonElem['CHAIR_ID2'], jsonElem['ID'])
+            listEmployee.append(listElem)
     else:
-        print('За ' + newsYear + ' год новостей нет!')
+        print('Список сотрудников, полученных по api, пуст!')
         sys.exit(1)
 else:
-    print('Ошибка api новостей вернуло код: ' + response.status_code)
+    print('Ошибка: api сотрудников вернуло код: ' + response.status_code)
     sys.exit(1)
-
+#==================================================================================
 
 conn = psycopg2.connect(
     database="news", 
@@ -62,13 +77,21 @@ conn = psycopg2.connect(
     port="5432"
 )
 
+#==================================================================================
+#cur = conn.cursor()
+#for el in listNews:
+#    query = "INSERT INTO public.news (id, url, title_orig, text_orig, news_date) VALUES (%s, %s, %s, %s, %s::TIMESTAMP)"
+#    data = (el.id, el.url, el.title_orig, el.text_orig, el.news_date)
+#    cur.execute(query, data)
+#    conn.commit()
 
 cur = conn.cursor()
-for el in listNews:
-    query = "INSERT INTO public.news (id, url, title_orig, text_orig, news_date) VALUES (%s, %s, %s, %s, %s::TIMESTAMP)"
-    data = (int(el.id), el.url, el.title_orig, el.text_orig, el.news_date)
+for el in listEmployee:
+    query = "INSERT INTO public.teachers (idperson, name, surname, patronymic, post, chair_id, chair_id2, id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    data = (el.idperson, el.name, el.surname, el.patronymic, el.post, el.chair_id, el.chair_id2, el.id)
     cur.execute(query, data)
     conn.commit()
+
 
 
 conn.close()
